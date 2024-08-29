14bf72d74b8b4048206084d3c334dc166e75ee38
Eduard <eddard1990@gmail.com>
2024-08-29 13:59:30 +0300

    finished footer desktop

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,6 +12,9 @@ import linkedin from "../../assets/mobile/linkedin.webp";
 import youtube from "../../assets/mobile/youtube.webp";
 import instagram from "../../assets/mobile/instagram.webp";
 import pinterest from "../../assets/mobile/pinterest.webp";
+import vectorDesk10 from "../../assets/desktop/vector_10.webp";
+import vectorDesk11 from "../../assets/desktop/vector_11.webp";
+import vectorDesk13 from "../../assets/desktop/vector_13.webp";
 
 export const Footer = () => {
   const windowWidth = useWindowSize();
@@ -21,9 +24,61 @@ export const Footer = () => {
     <footer className="footer">
       <div className="footer__img">
         <img src={footerLogo} alt="footer logo" className="footer__img-logo" />
-        <img src={vector4} alt="vector" className="footer__img-vector1" />
-        <img src={vector5} alt="vector" className="footer__img-vector2" />
+        <picture className="footer__img-vector1">
+          <source media="(max-width: 767px)" srcset={vector4} />
+          <source media="(min-width: 768px)" srcset={vectorDesk10} />
+          <img src={vector4} alt="vector 2" />
+        </picture>
+        <picture className="footer__img-vector2">
+          <source media="(max-width: 767px)" srcset={vector5} />
+          <source media="(min-width: 768px)" srcset={vectorDesk11} />
+          <img src={vector5} alt="vector 2" />
+        </picture>
       </div>
+      {!isMobileResolution && (
+        <>
+          <img src={vectorDesk13} alt="border" className="footer__border1" />
+          <nav className="footer__main-nav">
+            <div>
+              <ul>
+                <li>
+                  <a href="#">ABOUT</a>
+                </li>
+                <li>
+                  <a href="#">SERVICES</a>
+                </li>
+                <li>
+                  <a href="#">TECHNOLOGIES</a>
+                </li>
+                <li>
+                  <a href="#">HOW TO</a>
+                </li>
+                <li>
+                  <a href="#">JOIN HYDRA</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <img src={vectorDesk13} alt="border" className="footer__border2" />
+          <nav className="footer__help-nav">
+            <ul>
+              <li>
+                <a href="#">F.A.Q</a>
+              </li>
+              <li>
+                <a href="#">SITEMAP</a>
+              </li>
+              <li>
+                <a href="#">CONDITIONS</a>
+              </li>
+              <li>
+                <a href="#">LICENSES</a>
+              </li>
+            </ul>
+          </nav>
+          <img src={vectorDesk13} alt="border" className="footer__border3" />
+        </>
+      )}
       <div className="footer__social">
         <h3 className="footer__social-title">SOCIALIZE WITH HYDRA</h3>
         <div className="footer__social-link">
@@ -82,10 +137,17 @@ export const Footer = () => {
       </div>
       <div className="footer__border"></div>
       <div className="footer__copyright">
-        <p>
-          2023 © HYDRA LANDING PAGE <br /> BY ZINE. E. FALOUTI <br /> ALL RIGHTS
-          RESERVED{" "}
-        </p>
+        {isMobileResolution ? (
+          <p>
+            2023 © HYDRA LANDING PAGE <br /> BY ZINE. E. FALOUTI <br /> ALL
+            RIGHTS RESERVED
+          </p>
+        ) : (
+          <p>
+            2023 © HYDRA LANDING PAGE - BY ZINE. E. FALOUTI - ALL RIGHTS
+            RESERVED
+          </p>
+        )}
       </div>
     </footer>
   );
